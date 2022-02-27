4e517f931b8bc4ea0f851f9fd488019b2535f5f8
Nathan <nathan@nathan-s.com>
2022-02-27 15:51:05 -0800

    Add MongoDB

--- core/tasks/BaseTasks/BaseTask.py
+++ core/tasks/BaseTasks/BaseTask.py
@@ -1,11 +1,14 @@
 from celery import Task
 import os
 import redis
+import pymongo
+from urllib.parse import quote_plus
+from pymongo.database import Database
 
 
 class BaseTask(Task):
     _redis: redis.Redis = None
-    # _db = None
+    _db: pymongo.database.Database = None
 
     @property
     def redis(self) -> redis.Redis:
@@ -18,7 +21,17 @@ class BaseTask(Task):
             self._redis = redis.Redis(username=user, host=host, port=port, db=db, password=password)
         return self._redis
 
-    # @property
-    # def db(self):
-    #     return self._db
+    @property
+    def db(self) -> pymongo.database.Database:
+        if self._db is None:
+            user = os.environ["DbUser"]
+            password = os.environ["DbPassword"]
+            db = os.environ["DbName"]
+            host = os.environ["DbHost"]
+            port = int(os.environ["DbPort"])
+            uri = f"mongodb://{quote_plus(user)}:{quote_plus(password)}@{host}:{port}"
+            client = pymongo.MongoClient(uri, maxPoolSize=10)
+            self._db = client[db]
+        return self._db
+
 
