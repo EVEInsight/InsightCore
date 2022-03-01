51133389c13cb3d2124a0edaa8769f2c959b057b
Nathan <nathan@nathan-s.com>
2022-03-01 08:38:52 -0800

    Stream refactor.

--- core/models/Stream/Stream.py
+++ core/models/Stream/Stream.py
@@ -9,7 +9,7 @@ from pymongo.collection import ObjectId
 class Stream(BaseModel):
     config: Config
     filter: Filter
-    _id: str = None
+    id: str = None
 
     @classmethod
     def from_json(cls, dct: dict):
@@ -19,9 +19,21 @@ class Stream(BaseModel):
         :return: An instance of the class
         :rtype: Stream
         """
+        stream_id = dct.pop("_id", None)
         s = super().from_json(dct)
-        if isinstance(s._id, ObjectId):
-            s._id = str(s._id)
+        if stream_id is not None:
+            s.id = str(stream_id)
         s.config = Config.from_json(dct["config"])
         s.filter = Filter.from_json(dct["filter"])
         return s
+
+    def to_mongodb_json(self) -> dict:
+        """Returns a json dictionary with ID removed for inserts / updates into mongodb.
+
+        :return: Instance with id field removed.
+        """
+        d = self.to_json()
+        d.pop("id", None)
+        return d
+
+
--- core/tasks/API/CreateStream.py
+++ core/tasks/API/CreateStream.py
@@ -4,8 +4,8 @@ from core.tasks.BaseTasks.BaseTask import BaseTask
 from pymongo.collection import Collection
 
 
-@app.task(base=BaseTask, bind=True, max_retries=3, default_retry_delay=5, autoretry_for=(Exception,))
-def CreateStream(self, stream_json: dict) -> dict:
+@app.task(base=BaseTask)
+def CreateStream(stream_json: dict) -> dict:
     """Create a new stream and return the stream ID.
 
     :param self: Celery self reference required for retries.
@@ -17,10 +17,9 @@ def CreateStream(self, stream_json: dict) -> dict:
     if not isinstance(stream_json, dict):
         raise TypeError("stream_json must be of type dictionary")
     s = Stream.from_json(stream_json)
+    stream_json = s.to_mongodb_json()
 
-    streams: Collection = self.db.streams
-    stream_json = s.to_json()
-    stream_json.pop("_id")
+    streams: Collection = CreateStream.db.streams
     result = streams.insert_one(stream_json)
 
     return {"id": str(result.inserted_id)}
