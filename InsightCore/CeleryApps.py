2326c990330b3c41c9f6ef541ae8ad3ef61d51c5
Nathan <nathan@nathan-s.com>
2022-03-14 23:00:50 -0700

    Reuse init for celery app.

--- InsightCore/CeleryApps.py
+++ InsightCore/CeleryApps.py
@@ -1,6 +1,7 @@
 from ESICelery.CeleryApps import CeleryWorker, CeleryBeat
 from InsightCore.tasks.Pipeline import *
 from InsightCore.tasks.API import *
+import os
 
 
 class InsightCoreWorker(CeleryWorker):
@@ -14,7 +15,21 @@ class InsightCoreWorker(CeleryWorker):
         yield StreamFiltersStage2(), "PipelineStreamFiltersStage2"
         yield CreateStream(), "ApiCreateStream"
 
+    @classmethod
+    def create_class(cls):
+        """helper function to create an instance of the celery app reading the config variables from env variables.
 
-class InsightCoreBeat(CeleryBeat):
+        :return: An instance of the celery app wrapper class
+        :rtype: cls
+        """
+        c = cls(os.environ["BrokerUser"], os.environ["BrokerPassword"], os.environ["BrokerHost"],
+                int(os.environ["BrokerPort"]), os.environ["BrokerVhost"],
+                os.environ["ResultBackendUser"], os.environ["ResultBackendPassword"], os.environ["ResultBackendHost"],
+                int(os.environ["ResultBackendPort"]), int(os.environ["ResultBackendDb"]),
+                os.environ["HeaderEmail"])
+        return c
+
+
+class InsightCoreBeat(CeleryBeat, InsightCoreWorker):
     def tasks_to_register(self):
         yield GetMailRedisQ(), "PipelineGetMailRedisQ"
--- InsightCore/__main__.py
+++ InsightCore/__main__.py
@@ -4,17 +4,11 @@ import sys
 
 
 def main():
-    if "--beat" in sys.argv:
-        celery_server = InsightCoreBeat
-    else:
-        celery_server = InsightCoreWorker
     try:
-        c = celery_server(os.environ["BrokerUser"], os.environ["BrokerPassword"], os.environ["BrokerHost"],
-                          int(os.environ["BrokerPort"]), os.environ["BrokerVhost"],
-                          os.environ["ResultBackendUser"], os.environ["ResultBackendPassword"],
-                          os.environ["ResultBackendHost"],
-                          int(os.environ["ResultBackendPort"]), int(os.environ["ResultBackendDb"]),
-                          os.environ["HeaderEmail"])
+        if "--beat" not in sys.argv:
+            c = InsightCoreWorker.create_class()
+        else:
+            c = InsightCoreBeat.create_class()
     except KeyError as ex:
         print("Environmental variable is not set.")
         raise ex
