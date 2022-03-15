5b22b3a59e7bc9bdbbc1e1951fd6c66c63cf45a1
Nathan <nathan@nathan-s.com>
2022-03-14 17:45:28 -0700

    Simplify celery apps.

--- InsightCore/CeleryApps.py
+++ InsightCore/CeleryApps.py
@@ -1,50 +1,20 @@
 from ESICelery.CeleryApps import CeleryWorker, CeleryBeat
 from InsightCore.tasks.Pipeline import *
 from InsightCore.tasks.API import *
-import sys
-import os
 
 
-class CeleryLauncher(object):
-    @classmethod
-    def tasks(cls):
-        yield GetMailRedisQ()
-        yield ProcessMailEnqueueESICalls()
-        yield ProcessMailLoadFromESI()
-        yield EnqueueMailToStreams()
-        yield StreamFiltersStage1()
-        yield StreamFiltersStage2()
-        yield CreateStream()
+class InsightCoreWorker(CeleryWorker):
+    def tasks_to_register(self):
+        yield from super().tasks_to_register()
+        yield GetMailRedisQ(), "PipelineGetMailRedisQ"
+        yield ProcessMailEnqueueESICalls(), "PipelineProcessMailEnqueueESICalls"
+        yield ProcessMailLoadFromESI(), "PipelineProcessMailLoadFromESI"
+        yield EnqueueMailToStreams(), "PipelineEnqueueMailToStreams"
+        yield StreamFiltersStage1(), "PipelineStreamFiltersStage1"
+        yield StreamFiltersStage2(), "PipelineStreamFiltersStage2"
+        yield CreateStream(), "ApiCreateStream"
 
-    @classmethod
-    def run(cls):
-        if "--beat" in sys.argv:
-            celery_server = CeleryBeat
-        else:
-            celery_server = CeleryWorker
-        try:
-            c = celery_server(os.environ["BrokerUser"], os.environ["BrokerPassword"], os.environ["BrokerHost"],
-                              int(os.environ["BrokerPort"]), os.environ["BrokerVhost"],
-                              os.environ["ResultBackendUser"], os.environ["ResultBackendPassword"],
-                              os.environ["ResultBackendHost"],
-                              int(os.environ["ResultBackendPort"]), int(os.environ["ResultBackendDb"]),
-                              os.environ["HeaderEmail"])
-        except KeyError as ex:
-            print("Environmental variable is not set.")
-            raise ex
-        for t in cls.tasks():
-            c.register_task(t)
-            c.register_additional_queue(t.name)
-            c.register_task_route(t.name, t.name)
-        if isinstance(c, CeleryBeat):
-            s = {"task": "GetMailRedisQ",
-                 "schedule": 0.5,
-                 "options": {
-                     "ignore_result": True,
-                     "expires": 2
-                    }
-                 }
-            c.schedule_task("get-mail-redisq", s)
-            c.start()
-        else:
-            c.start(max_concurrency=int(os.environ["MaxConcurrency"]))
+
+class InsightCoreBeat(CeleryBeat):
+    def tasks_to_register(self):
+        yield GetMailRedisQ(), "PipelineGetMailRedisQ"
--- InsightCore/__main__.py
+++ InsightCore/__main__.py
@@ -1,5 +1,36 @@
-from InsightCore.CeleryApps import CeleryLauncher
+from InsightCore.CeleryApps import InsightCoreWorker, InsightCoreBeat
+import os
+import sys
+
+
+def main():
+    if "--beat" in sys.argv:
+        celery_server = InsightCoreBeat
+    else:
+        celery_server = InsightCoreWorker
+    try:
+        c = celery_server(os.environ["BrokerUser"], os.environ["BrokerPassword"], os.environ["BrokerHost"],
+                          int(os.environ["BrokerPort"]), os.environ["BrokerVhost"],
+                          os.environ["ResultBackendUser"], os.environ["ResultBackendPassword"],
+                          os.environ["ResultBackendHost"],
+                          int(os.environ["ResultBackendPort"]), int(os.environ["ResultBackendDb"]),
+                          os.environ["HeaderEmail"])
+    except KeyError as ex:
+        print("Environmental variable is not set.")
+        raise ex
+    if isinstance(c, InsightCoreBeat):
+        s = {"task": "GetMailRedisQ",
+             "schedule": 0.5,
+             "options": {
+                 "ignore_result": True,
+                 "expires": 2
+             }
+             }
+        c.schedule_task("get-mail-redisq", s)
+        c.start()
+    else:
+        c.start(max_concurrency=int(os.environ["MaxConcurrency"]))
 
 
 if __name__ == '__main__':
-    CeleryLauncher.run()
+    main()
